633631588487ec8a899ec8ebc31619764cc582e6
Yousuf Naveed Khan <129192243+YousufNaveedKhan@users.noreply.github.com>
2024-05-26 20:47:03 +0500

    Code Updated

--- RouteShowcase/RouteShowcase/Program.cs
+++ RouteShowcase/RouteShowcase/Program.cs
@@ -1,9 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "<h1>WELCOME</h1>");
-app.MapGet("/home" () => "<h2>This is Home page</h2>");
-app.MapGet("/about" () => "<h3>This is About page</h3>");
-app.MapGet("/contact" () => "Contact us at <b>contact@example.com</b>");
+app.MapGet("/", () => "Welcome");
+app.MapGet("/home" () => "This is Home page");
+app.MapGet("/about" () => "This is About page");
+app.MapGet("/contact" () => "Contact us at contact@example.com");
 
 app.Run();
