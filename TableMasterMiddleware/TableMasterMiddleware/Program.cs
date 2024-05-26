1feff1b9a757474ff9c68e410ddc81d533cad934
Yousuf Naveed Khan <129192243+YousufNaveedKhan@users.noreply.github.com>
2024-05-26 20:47:54 +0500

    Code Updated

--- TableMasterMiddleware/TableMasterMiddleware/Program.cs
+++ TableMasterMiddleware/TableMasterMiddleware/Program.cs
@@ -5,7 +5,7 @@ var app = builder.Build();
 
 app.Use(async (context, next) =>
 {
-    await context.Response.WriteAsync("Multiplication of Table 2:\n");
+    await context.Response.WriteAsync("<h2>Multiplication of Table 2:</h2>\n");
     for (int i = 1; i <= 10; i++)
     {
         await context.Response.WriteAsync($"2 x {i} = {2 * i}\n");
